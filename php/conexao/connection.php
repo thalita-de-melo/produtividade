b7ad399b4353ac4700535f6494074e4009b89bb5
Thalita de Melo <thalita.m.s@hotmail.com>
2022-06-29 14:41:20 -0300

    Conexão db
    db hostinger

--- php/conexao/connection.php
+++ php/conexao/connection.php
@@ -1,8 +1,8 @@
 <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "produtividade";
+  $servername = "localhost"; //localhost
+  $username = "u264392954_thalita"; //root
+  $password = "Admin2022"; 
+  $dbname = "u264392954_produtividade"; //produtividade
 
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
