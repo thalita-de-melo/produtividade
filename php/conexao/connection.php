1c686f3cf499d50b98b59cc4443a760377e37027
Thalita de Melo <thalita.m.s@hotmail.com>
2022-07-07 09:09:34 -0300

    Changes in connection

--- php/conexao/connection.php
+++ php/conexao/connection.php
@@ -1,8 +1,8 @@
 <?php
-  $servername = "localhost"; //localhost
-  $username = "root"; //root u264392954_thalita
-  $password = ""; //Admin2022
-  $dbname = "produtividade"; //produtividade u264392954_produtividade
+  $servername = "localhost"; 
+  $username = "root"; 
+  $password = ""; 
+  $dbname = "produtividade";
 
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
